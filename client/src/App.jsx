d4e48c1d3e9e7df3a788bac82849b941928d3803
Desmond Rogers <desmondrogers2@gmail.com>
2019-12-05 03:47:18 -0800

    Position sidebar and create module components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
 import Sidebar from './Sidebar';
+
+const Container = styled.div`
+  margin: auto;
+  height: 100%;
+  width: 1180px;
+  padding: 46px 0 30px;
+`;
+
+const Hero = styled.div`
+  height: 380px;
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  padding-top: 20px;
+  height: 100vh;
+`;
+
+const About = styled.div`
+  margin-right: 330px;
+  width: 850px;
+  `;
 
 export default () => (
   <div>
-    <Sidebar />
+    <Container>
+      <Hero />
+      <Wrapper>
+        <About />
+        <Sidebar />
+      </Wrapper>
+    </Container>
   </div>
 );
---
+++ client/src/Dashbox.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div` margin: 0 0 25px; `;
+
+const Box = styled.div`
+  background-color: #f2f2f2;
+  border: 1px solid #e5e5e5;
+  height: 173px;
+  padding: 38px;
+`;
+
+export default () => (
+  <Wrapper>
+    <article>
+      <Box />
+    </article>
+  </Wrapper>
+);
