08a20f19d918d5a76a85ca91009288004b45beb1
Desmond Rogers <desmondrogers2@gmail.com>
2019-12-10 17:18:43 -0800

    Add cors to requests

--- server/index.js
+++ server/index.js
@@ -7,6 +7,15 @@ app.use('/', express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
 
 app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.all('*', (req, res, next) => {
+  const origin = req.get('origin');
+  res.header('Access-Control-Allow-Origin', origin);
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/api/tracks/:id', (req, res) => {
   db.track.findOne({
     where: { id: req.params.id },
