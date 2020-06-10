0f5a292b9dc57795684ef738438a659cfd1deecb
Desmond Rogers <desmondrogers2@gmail.com>
2020-06-10 17:03:29 -0500

    Update sequelize db config

--- config/database.js
+++ config/database.js
@@ -2,8 +2,8 @@ module.exports = {
   development: {
     host: 'localhost',
     dialect: 'mysql',
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'password',
+    database: process.env.DB || 'kleio',
   },
 };
