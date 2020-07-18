5969b9da2b7a9cd6da4c29e8ebdcdbcbe91ed258
Desmond Rogers <desmondrogers2@gmail.com>
2020-07-18 04:13:06 -0500

    Update configs

---
+++ src/config/index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+const mysql = require('./database.js');
+const aws = require('./aws.js');
+
+const envFound = dotenv.config();
+if (envFound.error) {
+  // This error should crash whole process
+  throw new Error("⚠️ Couldn't find .env file  ⚠️");
+}
+
+module.exports = {
+  port: parseInt(process.env.PORT, 10),
+  mysql,
+  aws,
+};
