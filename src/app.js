e40c429226f29d2e9c9694adb1ef620e44c5e905
Desmond Rogers <desmondrogers2@gmail.com>
2020-07-18 03:31:53 -0500

    Fix models import path

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
-const db = require('../models');
+const db = require('./models');
 
 const app = express();
 app.use('/', express.static(path.resolve(__dirname, '..', 'public')));
