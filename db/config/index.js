4c6af7d76cff4390970985b89e31052425d5518d
Desmond Rogers <desmondrogers2@gmail.com>
2020-12-20 16:32:35 -0600

    update compose file for local dev

--- db/config/index.js
+++ db/config/index.js
@@ -1,22 +1,16 @@
-const fs = require('fs');
-
 module.exports = {
   development: {
     host: process.env.DB_HOST,
     dialect: 'mysql',
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
   },
-  production: (() => {
-    if (process.env.NODE_ENV === 'production') 
-      return {
-        dialect: 'mysql',
-        host: fs.readFileSync('/run/secrets/DB_HOST', 'utf8').trim(),
-        username: fs.readFileSync('/run/secrets/DB_USER', 'utf8').trim(),
-        password: fs.readFileSync('/run/secrets/DB_PASS', 'utf8').trim(),
-        database: fs.readFileSync('/run/secrets/DB_NAME', 'utf8').trim(),
-      }
-    else return;
-  })(),
+  production: {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
 };
--- server.js
+++ server.js
@@ -1,4 +1,11 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+
+const result = dotenv.config();
+
+if (result.error) {
+  throw result.error;
+}
 
 const PORT = process.env.PORT || 3004;
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
